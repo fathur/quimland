c92d8a02455ca1eb1bf97bc7afcea82f65377e47
Fathur Rohman <hi.fathur.rohman@gmail.com>
2026-07-07 16:57:42 +0700

    add static root

--- config/settings.py
+++ config/settings.py
@@ -93,4 +93,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', str(BASE_DIR / 'staticfiles'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
