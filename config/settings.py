129749aa3104f83c78670d75fcd2c84799f717ef
Fathur Rohman <hi.fathur.rohman@gmail.com>
2026-07-08 18:51:59 +0700

    add searchable user in payment batch

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_auto_filters',
     # 'mcp_server',
     'ql',
     'sorl.thumbnail',
--- ql/admin/payment_batch.py
+++ ql/admin/payment_batch.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from admin_auto_filters.filters import AutocompleteFilter
 from django import forms
 from django.contrib import admin, messages
 from django.db.models import Q, Sum
@@ -10,6 +11,11 @@ from django.utils.html import format_html
 from ql.utils import fmt_rupiah
 
 from ..models import Payment, PaymentBatch, Tariff
+
+
+class UserFilter(AutocompleteFilter):
+    title = 'User'
+    field_name = 'user'
 
 LOCK_AFTER_DAYS = 20
 
@@ -45,7 +51,7 @@ class PaymentInline(admin.TabularInline):
 @admin.register(PaymentBatch)
 class PaymentBatchAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'paid_at', 'nominal_display', 'receipt_icon', 'note']
-    list_filter = ['user', 'paid_at']
+    list_filter = [UserFilter, 'paid_at']
     search_fields = ['user__username', 'user__first_name', 'user__last_name']
     ordering = ['-paid_at']
     autocomplete_fields = ['user']
