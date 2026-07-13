cca84689760f5ea637a5b1bd31f971ff5327c62b
Fathur Rohman <hi.fathur.rohman@gmail.com>
2026-07-13 14:23:02 +0700

    fix filter

--- ql/admin/filters.py
+++ ql/admin/filters.py
@@ -1,12 +1,13 @@
-from datetime import date as date_type
+from datetime import datetime
 
 from django.contrib import admin
+from django.utils import timezone
 
 
 def make_date_range_filter(field_name, title=None):
     """
     Return a SimpleListFilter subclass that filters a DateTimeField by a from/to
-    date range using native <input type="date"> widgets in the sidebar.
+    datetime range using <input type="datetime-local"> widgets in the sidebar.
 
     Usage:
         @admin.register(MyModel)
@@ -18,7 +19,6 @@ def make_date_range_filter(field_name, title=None):
     filter_title = title or field_name.replace('_', ' ')
 
     class DateRangeFilter(admin.SimpleListFilter):
-        # parameter_name just needs to be unique; real filtering uses gte/lte params.
         parameter_name = gte_param
         template = 'admin/date_range_filter.html'
 
@@ -27,28 +27,41 @@ def make_date_range_filter(field_name, title=None):
             super().__init__(*args, **kwargs)
 
         def lookups(self, request, model_admin):
-            # Must be non-empty for Django to render the filter block.
             return (('_', '_'),)
 
         def queryset(self, request, queryset):
-            date_from = request.GET.get(gte_param, '').strip()
-            date_to   = request.GET.get(lte_param, '').strip()
+            dt_from = request.GET.get(gte_param, '').strip()
+            dt_to   = request.GET.get(lte_param, '').strip()
             try:
-                if date_from:
-                    queryset = queryset.filter(**{f'{field_name}__date__gte': date_type.fromisoformat(date_from)})
-                if date_to:
-                    queryset = queryset.filter(**{f'{field_name}__date__lte': date_type.fromisoformat(date_to)})
+                if dt_from:
+                    dt = datetime.fromisoformat(dt_from)
+                    if timezone.is_naive(dt):
+                        dt = timezone.make_aware(dt)
+                    queryset = queryset.filter(**{f'{field_name}__gte': dt})
+                if dt_to:
+                    dt = datetime.fromisoformat(dt_to)
+                    if timezone.is_naive(dt):
+                        dt = timezone.make_aware(dt)
+                    queryset = queryset.filter(**{f'{field_name}__lte': dt})
             except ValueError:
                 pass
             return queryset
 
         def choices(self, changelist):
+            # Collect all current params except our own so the form can re-emit
+            # them as hidden inputs, preserving other active filters on submit.
+            other_params = [
+                (k, v)
+                for k, v in changelist.params.items()
+                if k not in (gte_param, lte_param, 'p')
+            ]
             yield {
-                'date_from': changelist.params.get(gte_param, ''),
-                'date_to':   changelist.params.get(lte_param, ''),
+                'date_from':    changelist.params.get(gte_param, ''),   # ISO datetime string
+                'date_to':      changelist.params.get(lte_param, ''),   # ISO datetime string
                 'query_string': changelist.get_query_string(remove=[gte_param, lte_param]),
-                'gte_param': gte_param,
-                'lte_param': lte_param,
+                'gte_param':    gte_param,
+                'lte_param':    lte_param,
+                'other_params': other_params,
             }
 
     DateRangeFilter.__name__ = f'{field_name.title().replace("_", "")}DateRangeFilter'
