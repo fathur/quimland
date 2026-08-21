d2a1c96267bcb8b7cd22e9584fbc06bf8775e5d6
Fathur Rohman <hi.fathur.rohman@gmail.com>
2026-08-22 02:52:13 +0700

    search receipt by user

--- ql/fee/admin/receipt.py
+++ ql/fee/admin/receipt.py
@@ -15,7 +15,7 @@ class ReceiptAdmin(admin.ModelAdmin):
     list_filter   = ['storage']
     list_per_page = 12
     readonly_fields = ['storage', 'user', 'created_at', 'updated_at', 'deleted_at', 'image_preview']
-    search_fields = ['id', 'user']
+    search_fields = ['id', 'user__username', 'user__first_name', 'user__last_name']
 
     class Media:
         css = {'all': ['admin/css/receipt_grid.css']}
