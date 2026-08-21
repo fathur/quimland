6024967214c93c2df4738daecbb3150d26ddc550
Fathur Rohman <hi.fathur.rohman@gmail.com>
2026-08-22 02:48:39 +0700

    fix ui

--- ql/fee/admin/receipt.py
+++ ql/fee/admin/receipt.py
@@ -13,7 +13,7 @@ from ql.fee.models import Receipt
 class ReceiptAdmin(admin.ModelAdmin):
     list_display  = ['id', 'user', 'storage', 'image_preview', 'created_at']
     list_filter   = ['storage']
-    list_per_page = 10
+    list_per_page = 12
     readonly_fields = ['storage', 'user', 'created_at', 'updated_at', 'deleted_at', 'image_preview']
     search_fields = ['id']
 
